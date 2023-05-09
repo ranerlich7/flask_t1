d8d44030bc824a3e77401018c6770ec02f0cf7dc
ran <ran@goz.com>
2023-05-09 10:23:09 +0300

    fixed some bugs in basic app

--- app.py
+++ app.py
@@ -8,11 +8,11 @@ def hello_world():
    return "<p>Hello, World!</p>"
 
 @app.route("/news")
-def hello_world():
+def news():
    return "<p>This is the NEWS section!</p>"
 
-@app.route("/news")
-def hello_sport():
+@app.route("/sports")
+def sport():
    return "<p>This is the SPORT section!</p>"
 
 if __name__ == '__main__':
